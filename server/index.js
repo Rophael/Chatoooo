eeb79e2e2e642e25dbac4a92553379263d075683
Rophael <rofa214@gmail.com>
2023-10-14 14:59:54 +0300

    1 comm

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ const server = app.listen(process.env.PORT, () => {
 // Socket.io 
 const io = socket(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chatoooo.vercel.app',
         credentials: true,
     }
 });
